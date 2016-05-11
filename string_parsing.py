11043e98af195715975cbab77f818cbc107ac140
Andre Almar <andre@y7mail.com>
2016-05-11 17:21:25 -0300

    string parsing in Python

---
+++ string_parsing.py
@@ -0,0 +1,12 @@
+mystring = "This is a loooooong string, nah just kidding"
+
+my_list = mystring.split(" ")
+
+print(my_list)
+
+# Split a string on space and grab the 5th item in the list:
+# my_list = mystring.split(" ")[4]
+
+# transformar a lista em tupla
+# print(tuple(my_list))
+
